546664bac28c9ab60b3b2e2898dfbcc3098abc93
Alexander Eichner <alexander.eichner@oracle.com>
2025-11-21 13:02:45 +0000

    7.1: Backported r171503: GuestHost/DragAndDrop/DnDPath.cpp,GuestHost/SharedClipboard/clipboard-transfers.cpp,HostServices/SharedClipboard/testcase/tstClipboardTransfers.cpp: Properly split the path and check for .. references to allow transfering files which have a .. sequence in their name, github:gh-68
    svn:sync-xref-src-repo-rev: r171505

--- src/VBox/HostServices/SharedClipboard/testcase/tstClipboardTransfers.cpp
+++ src/VBox/HostServices/SharedClipboard/testcase/tstClipboardTransfers.cpp
@@ -1,4 +1,4 @@
-/* $Id: tstClipboardTransfers.cpp 106320 2024-10-15 12:08:41Z klaus.espenlaub@oracle.com $ */
+/* $Id: tstClipboardTransfers.cpp 111840 2025-11-21 13:02:45Z alexander.eichner@oracle.com $ */
 /** @file
  * Shared Clipboard transfers test case.
  */
@@ -405,8 +405,12 @@ static void testTransferObjOpen(RTTEST hTest)
     lstRoots.append(TESTTRANSFERROOTENTRY("my-transfer-1/dir1/sub1/file1.txt"));
     lstRoots.append(TESTTRANSFERROOTENTRY("my-transfer-1/dir2/file1.txt"));
     lstRoots.append(TESTTRANSFERROOTENTRY("my-transfer-1/dir2/sub1/file1.txt"));
+    lstRoots.append(TESTTRANSFERROOTENTRY("my-transfer-1/dir2/sub1/file2..txt"));
+    lstRoots.append(TESTTRANSFERROOTENTRY("my-transfer-1/dir2/sub1/file2...txt"));
 
     testTransferObjOpenSingle(hTest, lstRoots, "file1.txt", VINF_SUCCESS);
+    testTransferObjOpenSingle(hTest, lstRoots, "file2..txt", VINF_SUCCESS);
+    testTransferObjOpenSingle(hTest, lstRoots, "file2...txt", VINF_SUCCESS);
     testTransferObjOpenSingle(hTest, lstRoots, "does-not-exist.txt", VERR_PATH_NOT_FOUND);
     testTransferObjOpenSingle(hTest, lstRoots, "dir1/does-not-exist.txt", VERR_PATH_NOT_FOUND);
     testTransferObjOpenSingle(hTest, lstRoots, "../must-not-access-this.txt", VERR_INVALID_PARAMETER);
