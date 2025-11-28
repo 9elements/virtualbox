f5d78bba076db874161a4008b5e47842a49fd27e
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2025-11-28 17:46:00 +0000

    7.2: Backported r171610 r171611.
    * r171610: VBoxTray: Don't flood the error log with VERR_ACCESS_DENIED from VbglR3ClipboardReportFormats, that's perfectly natural if the clipboard sharing mode doesn't allow the guest to modify the host clipboard.
    * r171611: VBox/HostServices/VBoxClipboardSvc.h: Documented the VERR_ACCESS_DENIED conditions.
    
    
    svn:sync-xref-src-repo-rev: r171612

--- src/VBox/Additions/win/VBoxTray/VBoxClipboard.cpp
+++ src/VBox/Additions/win/VBoxTray/VBoxClipboard.cpp
@@ -1,4 +1,4 @@
-/* $Id: VBoxClipboard.cpp 111747 2025-11-14 16:43:28Z klaus.espenlaub@oracle.com $ */
+/* $Id: VBoxClipboard.cpp 111943 2025-11-28 17:46:00Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBoxClipboard - Shared clipboard, Windows Guest Implementation.
  */
@@ -425,7 +425,7 @@ static LRESULT vbtrShClWndProcWorker(PSHCLCONTEXT pCtx, HWND hwnd, UINT msg, WPA
                 }
             }
 
-            if (RT_FAILURE(rc))
+            if (RT_FAILURE(rc) && rc != VERR_ACCESS_DENIED /* mode */)
                 VBoxTrayError("Shared Clipboard: WM_CLIPBOARDUPDATE failed with %Rrc\n", rc);
 
             break;
