c67f4c7f0e446c6a65195ea4b9948939d4cc766f
Jack Doherty <jack.doherty@oracle.com>
2025-12-04 21:53:48 +0000

    7.1: libs/lwip: fix sequential ID in IP header. jiraref:VBP-1967
    svn:sync-xref-src-repo-rev: r171698

--- src/VBox/Devices/Network/lwip-new/src/core/ipv4/ip4.c
+++ src/VBox/Devices/Network/lwip-new/src/core/ipv4/ip4.c
@@ -836,12 +836,17 @@ err_t ip_output_if_opt(struct pbuf *p, ip_addr_t *src, ip_addr_t *dest,
 #if CHECKSUM_GEN_IP_INLINE
     chk_sum += iphdr->_len;
 #endif /* CHECKSUM_GEN_IP_INLINE */
+#ifdef VBOX
+    ip_id = (u16_t)RTRandU32();
+#endif
     IPH_OFFSET_SET(iphdr, 0);
     IPH_ID_SET(iphdr, htons(ip_id));
 #if CHECKSUM_GEN_IP_INLINE
     chk_sum += iphdr->_id;
 #endif /* CHECKSUM_GEN_IP_INLINE */
+#ifndef VBOX
     ++ip_id;
+#endif
 
     if (ip_addr_isany(src)) {
       ip_addr_copy(iphdr->src, netif->ip_addr);
