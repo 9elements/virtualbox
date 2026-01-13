d8e6ab87790d53e7a036afade2030ae1b1a0b5bd
Vitali Pelenjow <vitali.pelenjow@oracle.com>
2026-01-13 12:05:48 +0000

    7.1: backported r172164: Devices/Graphics: limit for element layout descriptions. bugref:11025
    svn:sync-xref-src-repo-rev: r172167

--- src/VBox/Devices/Graphics/DevVGA-SVGA3d-dx-dx11.cpp
+++ src/VBox/Devices/Graphics/DevVGA-SVGA3d-dx-dx11.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevVGA-SVGA3d-dx-dx11.cpp 112159 2025-12-17 22:05:17Z vitali.pelenjow@oracle.com $ */
+/* $Id: DevVGA-SVGA3d-dx-dx11.cpp 112461 2026-01-13 12:05:48Z vitali.pelenjow@oracle.com $ */
 /** @file
  * DevVMWare - VMWare SVGA device
  */
@@ -6103,6 +6103,9 @@ static void dxCreateInputLayout(PVGASTATECC pThisCC, PVMSVGA3DDXCONTEXT pDXConte
          * System-Value semantics ("SV_*") between shaders require proper names of course.
          * But they are irrelevant for input attributes.
          */
+        AssertCompile(RT_ELEMENTS(pEntry->descs) == RT_ELEMENTS(pDXElementLayout->aElementDesc));
+        ASSERT_GUEST_RETURN_VOID(pEntry->numDescs <= RT_ELEMENTS(pEntry->descs));
+
         pDXElementLayout->cElementDesc = pEntry->numDescs;
         for (uint32_t i = 0; i < pEntry->numDescs; ++i)
         {
