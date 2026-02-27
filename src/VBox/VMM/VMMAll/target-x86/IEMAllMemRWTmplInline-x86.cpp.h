90828dc17040add7e059f18bd06abcc89e960db0
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2026-02-27 08:51:37 +0000

    VMM/IEM/x86: iemMemStackPopGRegXxxxJmp should use iemMemApplySegmentToReadJmp rather than the write variant.
    svn:sync-xref-src-repo-rev: r172941

--- src/VBox/VMM/VMMAll/target-x86/IEMAllMemRWTmplInline-x86.cpp.h
+++ src/VBox/VMM/VMMAll/target-x86/IEMAllMemRWTmplInline-x86.cpp.h
@@ -1,4 +1,4 @@
-/* $Id: IEMAllMemRWTmplInline-x86.cpp.h 112403 2026-01-11 19:29:08Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMAllMemRWTmplInline-x86.cpp.h 113197 2026-02-27 08:51:37Z knut.osmundsen@oracle.com $ */
 /** @file
  * IEM - Interpreted Execution Manager - Inlined R/W Memory Functions Template, x86 target.
  */
@@ -1313,7 +1313,7 @@ RT_CONCAT3(iemMemStackPopGReg,TMPL_MEM_FN_SUFF,Jmp)(PVMCPUCC pVCpu, uint8_t iGRe
      */
     uint64_t      uNewRsp;
     RTGCPTR const GCPtrTop = iemRegGetRspForPop(pVCpu, sizeof(TMPL_MEM_TYPE), &uNewRsp);
-    RTGCPTR const GCPtrEff = iemMemApplySegmentToWriteJmp(pVCpu, X86_SREG_SS, sizeof(TMPL_MEM_TYPE), GCPtrTop);
+    RTGCPTR const GCPtrEff = iemMemApplySegmentToReadJmp(pVCpu, X86_SREG_SS, sizeof(TMPL_MEM_TYPE), GCPtrTop);
 #  if TMPL_MEM_TYPE_SIZE > 1
     if (RT_LIKELY(TMPL_MEM_ALIGN_CHECK(GCPtrEff)))
 #  endif
