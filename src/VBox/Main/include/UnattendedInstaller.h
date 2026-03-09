348e296fd52955101b38c98f0f0fb7e01397726b
Serkan Bayraktar <serkan.bayraktar@oracle.com>
2026-03-09 13:12:31 +0000

    7.2 Backported   r173008 Unattended ​bugref:11039 An attempt at fixing GA installation failures on test boxes.   r173011 Unattended ​bugref:11039 Fixing a malformed conditional in debian preseed.   r173013 Unattended ​bugref:11039 Fixing a malformed conditional in debian preseed. take 2.   r173018 Unattended ​bugref:9781 Some fixes on debian preseed and ubuntu user data files.   r173038 Unattended. ​bugref:9781 Fixing a typo in user data file.   r173040 Unattended. ​bugref:9781 ​bugref:11039 Now we use different post install scripts for Ubuntu and Debian since Debian one needs extra attention as to apt setup in no network, GA installation case.
    svn:sync-xref-src-repo-rev: r173047

--- src/VBox/Main/include/UnattendedInstaller.h
+++ src/VBox/Main/include/UnattendedInstaller.h
@@ -1,4 +1,4 @@
-/* $Id: UnattendedInstaller.h 112637 2026-01-19 11:42:20Z serkan.bayraktar@oracle.com $ */
+/* $Id: UnattendedInstaller.h 113293 2026-03-09 13:12:31Z serkan.bayraktar@oracle.com $ */
 /** @file
  * UnattendedInstaller class header
  */
@@ -648,7 +648,7 @@ public:
     DECLARE_TRANSLATE_METHODS(UnattendedUbuntuPreseedInstaller)
 
     UnattendedUbuntuPreseedInstaller(Unattended *pParent)
-        : UnattendedDebianInstaller(pParent, "ubuntu_preseed.cfg")
+        : UnattendedDebianInstaller(pParent, "ubuntu_preseed.cfg", "ubuntu_postinstall.sh")
     { Assert(!isOriginalIsoNeeded()); Assert(isAuxiliaryIsoNeeded()); Assert(!isAuxiliaryFloppyNeeded()); Assert(isAuxiliaryIsoIsVISO()); }
     ~UnattendedUbuntuPreseedInstaller() {}
 };
@@ -668,7 +668,7 @@ public:
     UnattendedUbuntuAutoInstallInstaller(Unattended *pParent)
         : UnattendedDebianInstaller(pParent,
                                     /* pszMainScriptTemplateName = */ "ubuntu_autoinstall_user_data",
-                                    /* pszPostScriptTemplateName = */ "debian_postinstall.sh",
+                                    /* pszPostScriptTemplateName = */ "ubuntu_postinstall.sh",
                                     /* pszMainScriptFilename     = */ "user-data")
     {
         Assert(!isOriginalIsoNeeded()); Assert(isAuxiliaryIsoNeeded());
