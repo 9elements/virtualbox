6051fb330928820785bdf6d6032db70c9f8db3c4
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2026-03-13 23:59:08 +0000

    Main/ConsoleImplTeleporter.cpp: Use initializer for szLine instead of RT_ZERO as it confused MSC /analyze. bugref:11070
    svn:sync-xref-src-repo-rev: r173159

--- src/VBox/Main/src-client/ConsoleImplTeleporter.cpp
+++ src/VBox/Main/src-client/ConsoleImplTeleporter.cpp
@@ -1,4 +1,4 @@
-/* $Id: ConsoleImplTeleporter.cpp 112403 2026-01-11 19:29:08Z knut.osmundsen@oracle.com $ */
+/* $Id: ConsoleImplTeleporter.cpp 113405 2026-03-13 23:59:08Z knut.osmundsen@oracle.com $ */
 /** @file
  * VBox Console COM Class implementation, The Teleporter Part.
  */
@@ -674,8 +674,7 @@ HRESULT Console::i_teleporterSrc(TeleporterStateSrc *pState)
     AssertRC(vrc);
 
     /* Read and check the welcome message. */
-    char szLine[RT_MAX(128, sizeof(g_szWelcome))];
-    RT_ZERO(szLine);
+    char szLine[RT_MAX(128, sizeof(g_szWelcome))] = {0};
     vrc = RTTcpRead(pState->mhSocket, szLine, sizeof(g_szWelcome) - 1, NULL);
     if (RT_FAILURE(vrc))
         return setErrorBoth(E_FAIL, vrc, tr("Failed to read welcome message: %Rrc"), vrc);
