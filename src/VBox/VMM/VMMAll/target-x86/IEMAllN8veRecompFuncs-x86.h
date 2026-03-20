3ba1489f483eaf65041199866c634bac9429453e
Bela Lubkin <bela.lubkin@oracle.com>
2026-03-20 06:30:34 +0000

    VMM/IEM: fix clang IEM VMM crash due to iemNativeEmitFinishInstructionWithStatus(); bugref:11078
    Empirically caused by the same sort of issue as in r173246; but I didn't
    actually drill down to disassembling the large compiled functions.
    
    Determined by replacing iemNativeEmitLoadGpr8Imm() calls with more or
    less equivalent iemNativeEmitLoadGprImm32() and observing that crashes
    were fixed; then bisecting down to the single culprit.
    
    Fix tested with clang 12 & 18 on Linux.
    
    svn:sync-xref-src-repo-rev: r173247

--- src/VBox/VMM/VMMAll/target-x86/IEMAllN8veRecompFuncs-x86.h
+++ src/VBox/VMM/VMMAll/target-x86/IEMAllN8veRecompFuncs-x86.h
@@ -1,4 +1,4 @@
-/* $Id: IEMAllN8veRecompFuncs-x86.h 112403 2026-01-11 19:29:08Z knut.osmundsen@oracle.com $ */
+/* $Id: IEMAllN8veRecompFuncs-x86.h 113485 2026-03-20 06:30:34Z bela.lubkin@oracle.com $ */
 /** @file
  * IEM - Native Recompiler - Inlined Bits, x86 target.
  */
@@ -510,7 +510,7 @@ iemNativeEmitFinishInstructionWithStatus(PIEMRECOMPILERSTATE pReNative, uint32_t
 
 #else
         /* Load the index as argument #1 for the helper call at the given label. */
-        off = iemNativeEmitLoadGpr8Imm(pReNative, off, IEMNATIVE_CALL_ARG1_GREG, idxTbLookupFirst);
+        off = iemNativeEmitLoadGprImm32(pReNative, off, IEMNATIVE_CALL_ARG1_GREG, idxTbLookupFirst);
 
         /*
          * Figure out the physical address of the current instruction and see
