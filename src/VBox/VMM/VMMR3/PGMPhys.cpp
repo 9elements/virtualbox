ccb99d5856e2b21ef2c652f29637a17c2709e44e
Aleksey Ilyushin <aleksey.ilyushin@oracle.com>
2026-04-06 14:45:53 +0000

    7.1: VMM/PGMPhys: More predictable memory accesses for PGMR3PhysReadExternal and PGMR3PhysWriteExternal. ​bugref:10572
    svn:sync-xref-src-repo-rev: r173503

--- src/VBox/VMM/VMMR3/PGMPhys.cpp
+++ src/VBox/VMM/VMMR3/PGMPhys.cpp
@@ -1,4 +1,4 @@
-/* $Id: PGMPhys.cpp 112005 2025-12-04 09:55:27Z alexander.eichner@oracle.com $ */
+/* $Id: PGMPhys.cpp 113733 2026-04-06 14:45:53Z aleksey.ilyushin@oracle.com $ */
 /** @file
  * PGM - Page Manager and Monitor, Physical Memory Addressing.
  */
@@ -178,7 +178,7 @@ VMMR3DECL(int) PGMR3PhysReadExternal(PVM pVM, RTGCPHYS GCPhys, void *pvBuf, size
                 int rc = pgmPhysGCPhys2CCPtrInternalReadOnly(pVM, pPage, pRam->GCPhys + off, &pvSrc, &PgMpLck);
                 if (RT_SUCCESS(rc))
                 {
-                    memcpy(pvBuf, pvSrc, cb);
+                    pgmPhysMemCopyWrapper<true>(pvBuf, pvSrc, cb);
                     pgmPhysReleaseInternalPageMappingLock(pVM, &PgMpLck);
                 }
                 else
@@ -321,7 +321,7 @@ VMMDECL(int) PGMR3PhysWriteExternal(PVM pVM, RTGCPHYS GCPhys, const void *pvBuf,
                 int rc = pgmPhysGCPhys2CCPtrInternal(pVM, pPage, pRam->GCPhys + off, &pvDst, &PgMpLck);
                 if (RT_SUCCESS(rc))
                 {
-                    memcpy(pvDst, pvBuf, cb);
+                    pgmPhysMemCopyWrapper<false>(pvDst, pvBuf, cb);
                     pgmPhysReleaseInternalPageMappingLock(pVM, &PgMpLck);
                 }
                 else
