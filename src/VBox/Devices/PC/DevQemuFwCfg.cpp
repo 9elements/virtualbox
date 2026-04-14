68053191ab68a812b2ad41ae9118c31828685029
Klaus Espenlaub <klaus.espenlaub@oracle.com>
2026-04-14 19:01:47 +0000

    7.2: Backported r173641: Devices/PC/DevQemuFwCfg.cpp: Typo fix for previous change, bugref:11071
    svn:sync-xref-src-repo-rev: r173642

--- src/VBox/Devices/PC/DevQemuFwCfg.cpp
+++ src/VBox/Devices/PC/DevQemuFwCfg.cpp
@@ -1,4 +1,4 @@
-/* $Id: DevQemuFwCfg.cpp 113865 2026-04-14 18:56:49Z klaus.espenlaub@oracle.com $ */
+/* $Id: DevQemuFwCfg.cpp 113868 2026-04-14 19:01:47Z klaus.espenlaub@oracle.com $ */
 /** @file
  * DevQemuFwCfg - QEMU firmware configuration compatible device.
  */
@@ -738,7 +738,7 @@ static DECLCALLBACK(int) qemuFwCfgR3ReadFileDir(PDEVQEMUFWCFG pThis, PCQEMUFWCFG
         AssertReturn(idxEntry < pThis->cCfgFiles, VERR_INTERNAL_ERROR);
 
         off %= sizeof(pThis->u.CfgFile);
-        cbToRead = RT_MAX(sizeof(pThis->u.CfgFile) - off);
+        cbToRead = RT_MAX(cbToRead, sizeof(pThis->u.CfgFile) - off);
         cbToRead = RT_MIN(cbToRead, sizeof(pThis->u.CfgFile));
 
         /* Setup the config file item. */
